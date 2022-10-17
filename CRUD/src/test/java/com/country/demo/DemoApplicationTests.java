19914d04526b93a416d97b78f7d957abe3293914
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-10-17 00:44:29 -0700

    commit to dev

---
+++ CRUD/src/test/java/com/country/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.country.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
