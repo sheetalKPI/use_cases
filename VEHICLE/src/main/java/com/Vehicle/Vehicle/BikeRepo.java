db00641d472998f0f7cff5ec9c77ee99fe681a1e
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-10-19 06:22:05 -0700

    commit

--- VEHICLE/src/main/java/com/Vehicle/Vehicle/BikeRepo.java
+++ VEHICLE/src/main/java/com/Vehicle/Vehicle/BikeRepo.java
@@ -1,10 +1,14 @@
 package com.Vehicle.Vehicle;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BikeRepo  extends JpaRepository<BikeEntity, Integer>{
 
-
+	@Query(value = "select * from bike b join car c on b.no_of_tyres = c.no_of_tyres")	
+	public List<CarEntity> getjoin();
  
 
 }
--- VEHICLE/src/main/java/com/Vehicle/Vehicle/CarRepo.java
+++ VEHICLE/src/main/java/com/Vehicle/Vehicle/CarRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 
 public interface CarRepo extends JpaRepository<CarEntity, Integer>{
-		@Query(value = "select * from truck t join car ")	
+		@Query(value = "select * from car c join truck t on c.no_of_tyres = t.no_of_tyres")	
 		public List<CarEntity> getjoin();
 }
