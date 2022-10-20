5290d91a08d35fa4ea6a754ad328e517afd5ef21
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-10-20 04:02:58 -0700

    commit

---
+++ Health/Health/src/test/java/com/Insurance/Health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insurance.Health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
